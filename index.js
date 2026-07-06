d204ed8757b0ec9ce8f3545990db83ea5434176a
EmmanuelUkaegbu <eukaegbu005@gmail.com>
2026-07-06 17:07:04 +0100

    updated cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const API = process.env.BASE_URL;
 const userRoute = require("./Routes/user.route.js");
