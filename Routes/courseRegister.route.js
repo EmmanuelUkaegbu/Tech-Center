924cd018be3a7f3b289b33295fac61e0c0aeb178
EmmanuelUkaegbu <eukaegbu005@gmail.com>
2026-07-08 08:43:46 +0100

    updated 6

--- Routes/courseRegister.route.js
+++ Routes/courseRegister.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middlewire/auth.js");
+const { protect, verifyToken } = require("../middlewire/auth.js");
 
 const {
   registerCourse,
